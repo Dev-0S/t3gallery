c20506772346bb3d7d1e3bdb4222159e9ec5edbd
Oliver Stanton <ostanton@Olivers-MBP.powerhub>
2024-11-17 17:32:50 +0000

    databse set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ const mockImages = mockUrls.map((url, index) => ({
 export default function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
+      <div className="flex flex-wrap justify-center gap-4">
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url}/>
