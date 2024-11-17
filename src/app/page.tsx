7bbe968e29b546d48a2773e560fca93762b465e2
Oliver Stanton <ostanton@Olivers-MBP.powerhub>
2024-11-17 21:26:41 +0000

    databse set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { imageConfigDefault } from "next/dist/shared/lib/image-config";
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/NfLxyFsanrmqNIW2VosanrmqkuDyLVfBIo9WhAF3jJzps8YZ",
@@ -13,10 +14,18 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }))
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts)
   return (
     <main className="">
       <div className="flex flex-wrap justify-center gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url}/>
