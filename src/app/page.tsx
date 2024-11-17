190cc00a31da71d45ca1bc601a2a212ad61d59c9
Oliver Stanton <ostanton@Olivers-MBP.powerhub>
2024-11-17 16:36:51 +0000

    ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+import { imageConfigDefault } from "next/dist/shared/lib/image-config";
 import Link from "next/link";
+
+const mockUrls = [
+  "https://utfs.io/f/NfLxyFsanrmqNIW2VosanrmqkuDyLVfBIo9WhAF3jJzps8YZ",
+  "https://utfs.io/f/NfLxyFsanrmqqOZwnSRhel8MnrpJGoZcF1DPfYyKz4AHxXiQ",
+  "https://utfs.io/f/NfLxyFsanrmqXUY4cvzinj4zQuNVrpkfa3CFcHZxlYy9W0qo",
+  "https://utfs.io/f/NfLxyFsanrmqRRTiZ1NUX2j6qKtTh8LdEN1v30OxkJZ457As",
+]
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}))
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#61a16a] to-[#02030f] text-white">
-        Hello World (Work in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url}/>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
