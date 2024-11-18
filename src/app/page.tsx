6e5a3733fef52b4a0233c3e82406c2e8e6eeea69
Oliver Stanton <ostanton@Olivers-MBP.powerhub>
2024-11-18 21:26:25 +0000

    db live plus images and urls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,33 +4,30 @@ import Link from "next/link";
 import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/NfLxyFsanrmqNIW2VosanrmqkuDyLVfBIo9WhAF3jJzps8YZ",
-  "https://utfs.io/f/NfLxyFsanrmqqOZwnSRhel8MnrpJGoZcF1DPfYyKz4AHxXiQ",
-  "https://utfs.io/f/NfLxyFsanrmqXUY4cvzinj4zQuNVrpkfa3CFcHZxlYy9W0qo",
-  "https://utfs.io/f/NfLxyFsanrmqRRTiZ1NUX2j6qKtTh8LdEN1v30OxkJZ457As",
-]
 
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}))
+// const mockUrls = [
+//   "https://utfs.io/f/NfLxyFsanrmqNIW2VosanrmqkuDyLVfBIo9WhAF3jJzps8YZ",
+//   "https://utfs.io/f/NfLxyFsanrmqqOZwnSRhel8MnrpJGoZcF1DPfYyKz4AHxXiQ",
+//   "https://utfs.io/f/NfLxyFsanrmqXUY4cvzinj4zQuNVrpkfa3CFcHZxlYy9W0qo",
+//   "https://utfs.io/f/NfLxyFsanrmqRRTiZ1NUX2j6qKtTh8LdEN1v30OxkJZ457As",
+// ]
+
+// const mockImages = mockUrls.map((url, index) => ({
+//   id: index + 1,
+//   url,
+// }))
 
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (images, { desc }) => [desc(images.id)]
+  });
 
-  console.log(posts)
+  console.log(images)
   return (
     <main className="">
       <div className="flex flex-wrap justify-center gap-4">
-        {posts.map((post) => (
-          <div className="" key={post.id}>{post.name} test</div>
-        ))}
-
-        {mockImages.map((image) => (
-          <div key={image.id} className="w-48">
-            <img src={image.url}/>
-          </div>
+        {images.map((image) => (
+          <div className="w-48 text-center" key={image.id}>{image.name} <img src={image.url}/> </div>
         ))}
       </div>
     </main>
