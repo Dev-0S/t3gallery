6161ecf8ed7fe90362dad6066c00b1bed7c7f5eb
Oliver Stanton <ostanton@Olivers-MBP.powerhub>
2024-11-18 22:21:24 +0000

    fix topnav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import "~/styles/globals.css";
 
 import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 
-import TopNav from "./_components/topnav";
-
+import TopNav from './_components/topnav';
+  
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
